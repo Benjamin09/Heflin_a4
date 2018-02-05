3813fc8b3be1a031dfdcf645613c52483729b2d5
Benjamin.Heflin <Benjamin.Heflin@pcc.edu>
2018-02-05 13:07:54 -0800

    added name brint out to Hello World out

--- src/Heflin/a4/helloWorld/HelloWorld.java
+++ src/Heflin/a4/helloWorld/HelloWorld.java
@@ -1,4 +1,12 @@
 package Heflin.a4.helloWorld;
 
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+        System.out.println("My name is:");
+        System.out.println("Benjamin Heflin");
+    }
+
 }
